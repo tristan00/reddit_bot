79e1e924f554195bcd8818ea99336a7e313b58fa
TD\tdelforge <tristandelforge1@gmail.com>
2017-10-03 07:24:09 -0700

    attempted post functionality

--- bot.py
+++ bot.py
@@ -6,13 +6,15 @@ import traceback
 import sqlite3
 import time
 
-bot = None
+main_bot = None
 creds = []
 sql_file = 'reddit_db.sqlite'
 bots = []
 
 #put in db
 rankings = {}
+
+class
 
 class post:
     def __init__(self, pid, data_url, comment_url, date_created, data_fullname, subreddit, title, score):
@@ -56,34 +58,66 @@ class bot:
         self.password = password
         self.session = get_session()
         self.sub = None
+        self.uh = None
 
     def login(self):
         try_counter = 3
         while try_counter >0:
-            time.sleep(5)
+            if (self.isloggedin()):
+                return 1
             try:
                 print(self.user, self.password)
                 login_data = {'api_type':'json','op':'login','passwd':self.password,'user':self.user}
                 login_url = 'https://www.reddit.com/api/login/d{0}'.format(self.user)
                 r = self.session.post(login_url, data = login_data)
                 time.sleep(1)
-                r = self.session.get('https://www.reddit.com')
-
-                if (self.user in r.text):
-                    return 1
-                break
             except:
                 self.session = get_session()
                 traceback.print_exc()
                 try_counter =- 1
         return 0
 
-    def post(self, text):
-        pass
+    def getmodhash(self):
+        r = self.session.get('https://www.reddit.com/api/me.json')
+        j = json.loads(r.text)
+        return j['data']['modhash']
 
+
+    def isloggedin(self):
+        r = self.session.get('https://www.reddit.com')
+        soup = BeautifulSoup(r.text, "html.parser")
+        #self.uh = soup.find('input',{'name':'uh', 'type':'hidden'})['value']
+        if (self.user in r.text):
+            return 1
+        else:
+            return 0
+
+
+    def post(self, subreddit, text, comment_page_url, parent_comment_id):
+        login_url = 'https://www.reddit.com/api/comment'
+
+        #read_url
+        r = self.session.get(comment_page_url)
+
+        soup = BeautifulSoup(r.text, "html.parser")
+
+        #thing_id=t1_dnufe8gtext=test43id=#commentreply_t1_dnufe8gr=howdoesredditworkuh=i8fn7btghc6e4f0adb3d2bfdf873256a51885084700a01b562renderstyle=html
+        #{'uh': 'hdzjn9f7i4d15e53e581e90c831a42a60359024e8e77091895', 'thing_id': 't1_dnufe8g', 'r': 'howdoesredditwork', 'c_id': '#commentreply_t1_dnufe8g', 'renderstyle': 'html'}
+
+        thing_id = 't1_' + parent_comment_id
+        c_id = '#commentreply_' + 't1_' +parent_comment_id
+        r = subreddit
+        uh = self.getmodhash()
+        renderstyle = 'html'
+
+        post_data = {'thing_id':thing_id,'c_id':c_id,'r':r,'uh': uh, 'renderstyle':renderstyle}
+        r = self.session.post(login_url, data = post_data)
+        print(post_data)
+        print(r.status_code)
 
 def create_bots():
     global creds
+    global main_bot
     conn = sqlite3.connect('reddit.db')
     c = conn.cursor()
     rs = c.execute('select * from reddit_logins').fetchall()
@@ -96,8 +130,8 @@ def create_bots():
     global bots
     for i in creds:
         print(i)
-
-        bots.append(bot(i, i['user_name'], i['password']))
+        main_bot = bot(i, i['user_name'], i['password'])
+        bots.append(main_bot)
 
     for b in bots:
         print(b, ' attempting login')
@@ -112,9 +146,9 @@ def get_session():
     return s
 
 def main():
-    global p_list
+    global main_bot
     create_bots()
-
+    main_bot.post('howdoesredditwork', 'test45', 'https://www.reddit.com/r/howdoesredditwork/comments/7408zb/test4/','dnufyvt')
 
 
 
